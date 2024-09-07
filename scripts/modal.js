48bcc3ebfe5e7818278ca7af95eba7122557df45
Deepinder Singh <gcod412@gmail.com>
2024-09-07 12:58:00 +0530

    Add files via upload

---
+++ scripts/modal.js
@@ -0,0 +1,170 @@
+$(document).ready(function() {
+  // MODAL
+  var modalText = {
+    discover: {
+      title: 'Maven Market Report ',
+      tag: 'Report Dashboard.',
+      detail:
+        '•	Developed interactive Power BI dashboards for AdventureWorks, focusing on sales trends, customer insights, and product performance',
+      link: 'https://app.powerbi.com/groups/me/reports/e539e4db-5554-43d2-b218-c63739750d2f?pbi_source=desktop'
+      
+    },
+    ordering: {
+      title: 'AdventureWork Report',
+      tag: '•	Gained valuable experience in transforming raw data into actionable insights.',
+      detail:
+        '•	Implemented DAX for custom metrics and ensured the dashboards were user-friendly for non-technical users',
+      link: ''
+    },
+    newrelic: {
+      title: 'Netflix Report',
+      tag: '•	Netflix Analytics',
+      detail:
+        'Points out that the report is focused on analyzing Netflix data, which is relevant to media and streaming industry professionals.',
+      link: 'https://app.powerbi.com/groups/me/reports/67ea2c63-6004-4727-8095-89853b79dd1f/0022328c660ada1991ac?experience=power-bi'
+    },
+    brave: {
+      title: 'Amazon Prime Report',
+      tag: 'Entertainment Analytics.',
+      detail:
+        'Demonstrates that the report is part of broader business intelligence efforts, focusing on extracting actionable insights from data',
+      link: ''
+    },
+    walker: {
+      title: 'Blinkit Report',
+      tag: 'E-commerce Metrics',
+      detail:
+        'Suggests that the report analyzes aspects of delivery efficiency, such as delivery times and fulfillment rates.',
+        link:''
+    },
+    powur: {
+      title: 'Powur.com',
+      tag: 'CONSUMER POWERED MARKETING.',
+      detail:
+        'Powur is a marketing platform for lead generation, recruitment, and team building. Built with Ruby on Rails and Angular-UI. Makes use of Angular-material for front-end visuals. Features complex user tree heiarchy and commission system.',
+      link: 'http://www.powur.com/with/42'
+    },
+    mystand: {
+      title: 'MyStand',
+      tag: 'CROWD-FUNDED CHARITY.',
+      detail:
+        'MyStand is a crowd-funding, media sharing website, that has you donating actions instead of money out of your pocket. Single page App built with Node.js on Sails and Angular 2.0. Features social media sharing and large scale crowd-funding.'
+    },
+    never: {
+      title: 'NeverSurrender',
+      tag: 'ALS AWARENESS.',
+      detail:
+        'NeverSurrender is a platform for the new ALS foundation mobile app in hopes to raise awareness and research funding to fight ALS. Pure JavaScript marketing site to promote the new ALS NeverSurrender app.'
+    },
+    themall: {
+      title: 'The Mall',
+      tag: 'PEER GUIDED SHOPPING.',
+      detail:
+        'The Mall is a place to follow the latest fashion purchases of your friends and favorite celebrities. Built with Node.js and Handlebars. Features the ability to import thousands of top brands products into one shopping site.'
+    }
+  };
+
+  $('#gallery .button').on('click', function() {
+    fillModal(this.id);
+    $('.modal-wrap').addClass('visible');
+  });
+
+  $('.close').on('click', function() {
+    $('.modal-wrap, #modal .button').removeClass('visible');
+  });
+
+  $('.mask').on('click', function() {
+    $('.modal-wrap, #modal .button').removeClass('visible');
+  });
+
+  var carousel = $('#carousel'),
+    slideWidth = 700,
+    threshold = slideWidth / 3,
+    dragStart,
+    dragEnd;
+
+  setDimensions();
+
+  $('#next').click(function() {
+    shiftSlide(-1);
+  });
+  $('#prev').click(function() {
+    shiftSlide(1);
+  });
+
+  carousel.on('mousedown', function() {
+    if (carousel.hasClass('transition')) return;
+    dragStart = event.pageX;
+    $(this).on('mousemove', function() {
+      dragEnd = event.pageX;
+      $(this).css('transform', 'translateX(' + dragPos() + 'px)');
+    });
+    $(document).on('mouseup', function() {
+      if (dragPos() > threshold) {
+        return shiftSlide(1);
+      }
+      if (dragPos() < -threshold) {
+        return shiftSlide(-1);
+      }
+      shiftSlide(0);
+    });
+  });
+
+  function setDimensions() {
+    if (
+      /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+        navigator.userAgent
+      )
+    ) {
+      slideWidth = $(window).innerWidth();
+    }
+    $('.carousel-wrap, .slide').css('width', slideWidth);
+    $('.modal').css('max-width', slideWidth);
+    $('#carousel').css('left', slideWidth * -1);
+  }
+
+  function dragPos() {
+    return dragEnd - dragStart;
+  }
+
+  function shiftSlide(direction) {
+    if (carousel.hasClass('transition')) return;
+    dragEnd = dragStart;
+    $(document).off('mouseup');
+    carousel
+      .off('mousemove')
+      .addClass('transition')
+      .css('transform', 'translateX(' + direction * slideWidth + 'px)');
+    setTimeout(function() {
+      if (direction === 1) {
+        $('.slide:first').before($('.slide:last'));
+      } else if (direction === -1) {
+        $('.slide:last').after($('.slide:first'));
+      }
+      carousel.removeClass('transition');
+      carousel.css('transform', 'translateX(0px)');
+    }, 700);
+  }
+
+  function fillModal(id) {
+    $('#modal .title').text(modalText[id].title);
+    $('#modal .detail').text(modalText[id].detail);
+    $('#modal .tag').text(modalText[id].tag);
+    if (modalText[id].link)
+      $('#modal .button')
+        .addClass('visible')
+        .parent()
+        .attr('href', modalText[id].link);
+
+    $.each($('#modal li'), function(index, value) {
+      $(this).text(modalText[id].bullets[index]);
+    });
+    $.each($('#modal .slide'), function(index, value) {
+      $(this).css({
+        background:
+          "url('img/slides/" + id + '-' + index + ".jpg') center center/cover",
+        backgroundSize: 'cover'
+      });
+    });
+  }
+});
